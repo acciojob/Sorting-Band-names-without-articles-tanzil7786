00ff0d6262be016569c8245818143743168dd8f5
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-05-21 15:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//your code here
+function strip (word) {
+let regex = new RegExp('^(a |the |an )', 'i')
+return word.replace(regex, '').trim()
+}
 
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+document.querySelector('#bands').innerHTML =
+sortedBands.map(item => <li>${item}</li>).join('')
